399b26eb3bee4485bd940c8e65f4a1a29956a431
jstasko <jozef.stasko@ringier.sk>
2019-05-07 10:38:27 +0200

    commit

---
+++ NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jozef.stasko
+ * Date: 2019-05-06
+ * Time: 11:33
+ */
+
+namespace Validation;
+
+
+class NotFoundException extends \Exception
+{
+
+}
---
+++ Validation.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jozef.stasko
+ * Date: 2019-05-02
+ * Time: 19:06
+ */
+
+namespace Validation;
+
+
+use Validation\NotFoundException;
+
+class Validation
+{
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function ridOfWhiteSpacies(string $str): string
+    {
+        return preg_replace('/\s+/', ' ', $str);
+    }
+
+    /**
+     * @param array $pokemonAttributes
+     * @return array
+     * @throws NotFoundException
+     */
+    public static function validationOfPokemon(array $pokemonAttributes): array
+    {
+        if (isset($pokemonAttributes['name']) && isset($pokemonAttributes['order'])
+            && isset($pokemonAttributes['height']) && isset($pokemonAttributes['weight'])
+            && isset($pokemonAttributes['sprites']['front_default'])) {
+            return [
+                'name' => $pokemonAttributes['name'],
+                'pokedexNumber' => $pokemonAttributes['id'],
+                'height' => $pokemonAttributes['height'],
+                'weight' => $pokemonAttributes['weight'],
+                'image' => $pokemonAttributes['sprites']['front_default']
+            ];
+        }
+        throw new NotFoundException('Pokemon was not found');
+    }
+
+    /**
+     * @param array $abilities
+     * @return array
+     * @throws NotFoundException
+     */
+    public static function validationOfPokemonsAbilities(array $abilities): array
+    {
+        if (!empty($abilities)) {
+            return $abilities;
+        }
+        throw new NotFoundException('Abilities dont exist');
+
+    }
+
+    /**
+     * @param array $types
+     * @return array
+     * @throws NotFoundException
+     */
+    public static function validationOfPokemonsTypes(array $types): array
+    {
+        if (!empty($types)) {
+            return $types;
+        }
+        throw new NotFoundException('Type doesnt exist');
+
+    }
+
+    /**
+     * @param array $stats
+     * @return array
+     * @throws NotFoundException
+     */
+    public static function validationOfPokemonsStats(array $stats): array
+    {
+        if (!empty($stats)) {
+            return $stats;
+        }
+        throw new NotFoundException('Stats dont exists');
+
+    }
+}
